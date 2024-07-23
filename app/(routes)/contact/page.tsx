37fa590445d96b0e1f9fc2855a0aaf10996d7219
KDeniz <keremefeyenideniz@gmail.com>
2024-07-23 16:11:13 +0300

    contact ui fully done

--- app/(routes)/contact/page.tsx
+++ app/(routes)/contact/page.tsx
@@ -8,58 +8,57 @@ export default function Contact() {
     return (
         <div className="p-2 w-full">
             <div className="p-12 w-full items-center justify-center flex">
-                <div className="max-w-6xl grid grid-cols-5 w-full space-x-3 relative "> 
-                    <div className="col-span-3 grid w-full px-12 pt-12 shadow-lg shadow-gray-800">
-                        <div className="text-4xl font-bold pt-8"> 
-                            Company Details
+                <div className="max-w-6xl grid grid-cols-5 w-full space-x-3 "> 
+                    <div className="col-span-3 row-span-2 grid w-full pl-12 pr-2">
+                        <div className="shadow-lg shadow-gray-800 p-12">
+                            <div className="text-4xl font-bold py-8"> 
+                                Company Details
+                            </div>
+                            <div className="grid grid-cols-2">
+                                <div className="">
+                                    <div className="text-2xl py-6">
+                                        <Phone className="inline text-2xl pr-3 "/>
+                                        <div className="inline h-[40px] aspect-square">Phone Number</div>
+                                    </div>
+                                    <div className="text-xl">+123 456 789 101</div>
+                                </div>
+                                <div>
+                                    <div className="text-2xl py-6">
+                                        <MailOpen className="inline text-2xl pr-3 "/>
+                                        <div className="inline h-[40px] aspect-square">E-Mail Address</div>
+                                    </div>
+                                    <div className="text-xl underline">kebabmaster@gmail.com</div>
+                                </div>
+                            </div>
+                            <div className="grid grid-cols-2 pt-8 ">
+                                <div>
+                                    <div className="text-2xl pb-6">
+                                        <MapPin className="inline text-2xl pr-3 "/>
+                                        <div className="inline h-[40px] aspect-square">Location</div>
+                                    </div>
+                                    <div className="text-xl">123 Kebab Street <br></br>Netherlands, NL 10001</div>
+                                </div>
+                                <div>
+                                    <div className="text-2xl pb-6">
+                                        <Share2  className="inline text-2xl pr-3 "/>
+                                        <div className="inline h-[40px] aspect-square">Connect with us</div>
+                                    </div>
+                                    <div className="text-xl">
+                                        <Facebook className="inline space-x-2"/>
+                                        <Twitter className="inline ml-2"/>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
-                        <div className="grid grid-cols-2">
-                            <div className="">
-                                <div className="text-2xl pb-6">
-                                    <Phone className="inline text-2xl pr-3 "/>
-                                    <div className="inline h-[40px] aspect-square">Phone Number</div>
-                                </div>
-                                <div className="text-xl">+123 456 789 101</div>
-                            </div>
-                            <div>
-                                <div className="text-2xl pb-6">
-                                    <MailOpen className="inline text-2xl pr-3 "/>
-                                    <div className="inline h-[40px] aspect-square">E-Mail Address</div>
-                                </div>
-                                <div className="text-xl underline">kebabmaster@gmail.com</div>
-                            </div>
-                        </div>
-                        <div className="grid grid-cols-2 pt-8">
-                            <div>
-                                <div className="text-2xl pb-6">
-                                    <MapPin className="inline text-2xl pr-3 "/>
-                                    <div className="inline h-[40px] aspect-square">Location</div>
-                                </div>
-                                <div className="text-xl">123 Kebab Street <br></br>Netherlands, NL 10001</div>
-                            </div>
-                            <div>
-                                <div className="text-2xl pb-6">
-                                    <Share2  className="inline text-2xl pr-3 "/>
-                                    <div className="inline h-[40px] aspect-square">Connect with us</div>
-                                </div>
-                                <div className="text-xl">
-                                    <Facebook className="inline space-x-2"/>
-                                    <Twitter className="inline ml-2"/>
-                                </div>
-                            </div>
-                        </div>
-                       
-                    </div>
-                    <div className="col-span-3 row-span-1 row-start-4">
-
                         <Image 
                             src={"/netherlands.PNG"} 
                             alt={"left"}            
                             width={500}
                             height={500}
-                            className="w-full h-full"
+                            className="w-full h-full pt-4"
                             />
                     </div>
+                    
                     <div className="col-span-2 bg-[#cf0a2c] p-12 text-white">
                         <div className="text-4xl font-semibold py-8">
                             Get in Thouch
